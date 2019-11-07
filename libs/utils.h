7973456c4bca460d4f1a1c8036fa798e27b154b2
Julien Geneste <jgeneste001@enseirb-matmeca.fr>
2019-11-07 11:59:03 +0100

    Fonctions read_header & recive_message relocated

--- libs/utils.h
+++ libs/utils.h
@@ -5,12 +5,13 @@
 
 #define SERV_PIPE_NAME "/tmp/chat"
 #define MAX_USER_NAME_LEN 20
+#define CMD_SERVER 0x2
 struct user {
     int id;
     char name[MAX_USER_NAME_LEN];
 };
 
-struct message{
+struct message {
     int user_id;
     int data_len;
     char *data;
@@ -24,4 +25,7 @@ void exit_if(int condition, const char *prefix);
 
 int four_char_to_int(char *bytes);
 void int_to_four_char(int n, char *bytes);
+
+struct message read_header(int fd);
+void recive_mesrsage(int fd, char * data, int data_len);
 #endif
