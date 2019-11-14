a2d8d6aa48e7647448c46e5250c1f103be29db13
CLUZEL Baptiste <baptiste.cluzel@enseirb.fr>
2019-11-14 19:41:15 +0100

    EOC done

--- server/src/main.h
+++ server/src/main.h
@@ -7,8 +7,9 @@ struct user_bank {
 struct message read_header(int fd);
 void recive_message(int fd, char * data, int data_len);
 void process_hello(struct user_bank *connected_users, unsigned int user_id);
-unsigned int search_user(struct user_bank *connected_users, unsigned int user_id);
+int search_user(struct user_bank *connected_users, unsigned int user_id);
 int add_user(struct user_bank *connected_users, unsigned int user_id);
 int delete_user(struct user_bank *connected_users, unsigned int user_id);
 int change_user_name(struct user_bank *connected_users, unsigned int user_id, char * username);
 int open_client_connection(unsigned int user_id);
+void broadcast_str(struct user_bank *users, char * message);
